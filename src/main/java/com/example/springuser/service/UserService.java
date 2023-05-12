7f665371f4160dcc8a4c4d424231f562c7aeb99b
soongon <soongon@gmail.com>
2023-05-12 15:54:15 +0900

    initial commit

--- src/main/java/com/example/springuser/service/UserService.java
+++ src/main/java/com/example/springuser/service/UserService.java
@@ -7,6 +7,6 @@ public class UserService {
 
     // 사용자 전제를 조회하는 기능
     public String getAllUsers() {
-        return "전체 사용자 목록 입니다. from service";
+        return "전체 사용자 목록 입니다. from service..";
     }
 }
